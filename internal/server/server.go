cf46574c7ab3199084e0d4014ed0783e65ff2680
shaishav-rgb <shaishavpaudel@gmail.com>
2026-05-27 07:47:37 +0545

    chore: cleanup code

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,15 +2,12 @@ package server
 
 import (
 	"context"
-	"fmt"
-	"net"
 
 	"buf.build/go/protovalidate"
 	"github.com/Auction-Application/be-user-service/internal/database"
 	"github.com/Auction-Application/be-user-service/internal/database/usersTableQuery"
 	userPb "github.com/Auction-Application/be-user-service/rpc/gen/user/v1"
 	"github.com/jackc/pgx/v5"
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -45,27 +42,27 @@ func NewUserServer() *UserServer {
 	}
 }
 
-func Run(port int) error {
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+// func Run(port int) error {
+// 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 
-	if err != nil {
-		fmt.Println("failed to start the server")
-		return err
-	}
-	defer listener.Close()
+// 	if err != nil {
+// 		fmt.Println("failed to start the server")
+// 		return err
+// 	}
+// 	defer listener.Close()
 
-	grpcServer := grpc.NewServer()
+// 	grpcServer := grpc.NewServer()
 
-	ctx := context.Background()
-	userServer := NewUserServer()
+// 	ctx := context.Background()
+// 	userServer := NewUserServer()
 
-	defer userServer.databaseConn.Close(ctx)
+// 	defer userServer.databaseConn.Close(ctx)
 
-	userPb.RegisterUserServiceServer(grpcServer, userServer)
+// 	userPb.RegisterUserServiceServer(grpcServer, userServer)
 
-	if err := grpcServer.Serve(listener); err != nil {
-		fmt.Println("error listening the server")
-		return err
-	}
-	return nil
-}
+// 	if err := grpcServer.Serve(listener); err != nil {
+// 		fmt.Println("error listening the server")
+// 		return err
+// 	}
+// 	return nil
+// }
